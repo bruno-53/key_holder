c335873a2cb3b3c917a98f91507d1437b64ee5c6
Bruno Aparecido <brunoaparecidoalves@gmail.com>
2020-05-26 21:07:20 -0300

    NEW CLEAN MAIN

--- src/main.py
+++ src/main.py
@@ -1,25 +1,11 @@
-import os
-from getpass import getpass
-from views.menu import main_menu
-from views.data_base import create_table
+from model.data_base import create_table
+from model.model import first_autentication
 
-create_table()
 
 print("********************\n")
 print("-KEY HOLDER v 0.0.1-\n")
 print("********************")
 
-master_key = '123'
+create_table()
+first_autentication()
 
-# GETTING KEY IN HIDDEN MODE
-key = getpass(prompt='Insert your master key: ')
-
-
-# MASTER KEY VALIDATION
-if key != master_key:
-	print ('\n!!Wrong key!!\n')
-	exit()
-
-else:
-	os.system('cls' if os.name == 'nt' else 'clear')
-	main_menu()
